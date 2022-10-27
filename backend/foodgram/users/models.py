788daa96b2111115175abc5ea963541afa511643
Andrey Mac <linnaip@yandex.ru>
2022-10-27 20:47:26 +0300

    Прописана модель user

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Модель пользователя.
+    """
+    ADMIN = 'admin'
+    USER = 'user'
+    ROLES = [
+        (ADMIN, 'Administrator'),
+        (USER, 'User'),
+    ]
+    username = models.CharField(
+        max_length=150,
+        null=False,
+        blank=False,
+        unique=True
+    )
+    first_name = models.CharField(
+        max_length=200
+    )
+    last_name = models.CharField(
+        max_length=200
+    )
+    email = models.EmailField(
+        max_length=255,
+        blank=False,
+        null=False,
+        unique=True
+    )
+    role = models.CharField(
+        max_length=50,
+        null=True,
+        choices=ROLES,
+        default=USER
+    )
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(fields=['username', 'email'],
+                                    name='uniq_signup')
+        )
