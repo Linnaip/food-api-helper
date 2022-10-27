843b4cf9ddffc5cb50d6848dead381c9d22f2086
Andrey Mac <linnaip@yandex.ru>
2022-10-27 08:35:47 +0300

    Дописаны поля для recipes

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,14 +1,14 @@
 from colorfield.filds import ColorField
 from django.db import models
 
+
 class Tags(models.Model):
     """
     Модель тэгов.
     """
     name = models.CharField(max_length=200)
-    color = ColorField(default='#FF0000')
-    #slug =
-    pass
+    color = ColorField(default='#FF0000', unique=True)
+    slug = models.SlugField(unique=True)
 
 
 class Ingredients(models.Model):
@@ -27,22 +27,28 @@ class Recipes(models.Model):
     """
     Главная модель рецептов.
     """
-    #ingredients = models.ForeignKey(
-#        Ingredients,
-#       on_delete=models.CASCADE,
-#       related_name='recipes'
+    ingredients = models.ManyToManyField(
+        Ingredients,
+        through='RecipeIngredients',
+        related_name='recipes'
     )
     tags = models.ForeignKey(
         Tags,
         on_delete=models.CASCADE,
         related_name='recipes'
     )
-    #pub_date=
-    #image=
-    #name = models.CharField(max_length=200)
-    #text=
-    #cooking_time=
-    #author=
+    pub_date = models.DateField('Дата публикации', auto_now_add=True)
+    image = models.ImageField()
+    name = models.CharField(max_length=200)
+    text = models.CharField(max_length=500)
+    cooking_time = models.PositiveSmallIntegerField(
+        # validator
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
 
 
 class RecipeIngredients(models.Model):
@@ -54,9 +60,12 @@ class RecipeIngredients(models.Model):
         on_delete=models.CASCADE,
         related_name='recipe_ingredients'
     )
-    recipe= models.ForeignKey(
+    recipe = models.ForeignKey(
         Recipes,
         on_delete=models.CASCADE,
         related_name='recipe_ingredients'
     )
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(
+        default=1,
+        # validator
+    )
